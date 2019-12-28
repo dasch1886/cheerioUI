d085277692a6370485ad0dddc31775539520cf0c
dasch1886 <kamil.kopacz09@gmail.com>
2019-12-28 13:15:38 +0100

    added filter form group with appropriate methods

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -5,6 +5,8 @@ import { RecipeService } from './../../core/http/recipe/recipe.service';
 import { RecipesListModel } from '../../shared/models/recipe-list.model';
 import { RecipeModel } from 'src/app/shared/models/recipe.model';
 import { pluck } from 'rxjs/operators';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { SpinnerService } from 'src/app/core/services/spinner.service';
 
 @Component({
   selector: 'app-home',
@@ -12,13 +14,21 @@ import { pluck } from 'rxjs/operators';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit, AfterViewInit {
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+    private fb: FormBuilder,
+    private preloader: SpinnerService) { }
 
   recipes$: Observable<RecipesListModel>
   recipesListSize$: Observable<number>;
   recipesArray$: Observable<Array<RecipeModel>>;
+  private filterGroup: FormGroup;
 
   ngOnInit() {
+    this.filterGroupInit();
+    this.getRecipes();
+  }
+
+  getRecipes() {
     this.recipes$ = from(this.recipeService.findAll());
     this.recipesArray$ = this.recipes$.pipe(pluck('recipes'));
     this.recipesListSize$ = this.recipes$.pipe(pluck('listSize'));
@@ -31,5 +41,17 @@ export class HomeComponent implements OnInit, AfterViewInit {
       easing: 'ease-out',
       mirror: true
     });
+  }
+
+  filterGroupInit() {
+    this.filterGroup = this.fb.group({
+      text: '',
+    });
+  }
+
+  filterRecipes() {
+    this.recipesArray$ = this.recipeService.filterByText(
+      this.filterGroup.get('text').value
+    );
   }
 }
