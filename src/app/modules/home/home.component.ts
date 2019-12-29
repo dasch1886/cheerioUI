ae9575195ac64423b61eb62d12d18f733d2aeb73
dasch1886 <kamil.kopacz09@gmail.com>
2019-12-29 22:56:32 +0100

    added containers for advanced filter

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import AOS from 'aos';
-import { Observable, from } from 'rxjs';
+import { Observable, from, Subject } from 'rxjs';
 import { RecipeService } from './../../core/http/recipe/recipe.service';
 import { RecipesListModel } from '../../shared/models/recipe-list.model';
 import { RecipeModel } from 'src/app/shared/models/recipe.model';
 import { pluck } from 'rxjs/operators';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { SpinnerService } from 'src/app/core/services/spinner.service';
-
+import { IngredientService } from 'src/app/core/http/ingredient/ingredient.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -16,12 +15,18 @@ import { SpinnerService } from 'src/app/core/services/spinner.service';
 export class HomeComponent implements OnInit, AfterViewInit {
   constructor(private recipeService: RecipeService,
     private fb: FormBuilder,
-    private preloader: SpinnerService) { }
+    private ingredientService: IngredientService) { }
+
+  eventsSubject: Subject<string> = new Subject<string>();
 
   recipes$: Observable<RecipesListModel>;
   recipesListSize$: Observable<number>;
   recipesArray$: Observable<Array<RecipeModel>>;
+  authors: Array<string> = [];
+  ingredients: Array<string> = [];
   private filterGroup: FormGroup;
+
+  ingrdeintsToFilter: Array<string> = [];
 
   ngOnInit() {
     this.filterGroupInit();
@@ -32,6 +37,20 @@ export class HomeComponent implements OnInit, AfterViewInit {
     this.recipes$ = from(this.recipeService.findAll());
     this.recipesArray$ = this.recipes$.pipe(pluck('recipes'));
     this.recipesListSize$ = this.recipes$.pipe(pluck('listSize'));
+    this.setAuthors();
+    this.setIngredietns();
+  }
+
+  setAuthors() {
+    this.recipeService.getAuthors().toPromise().then(res => {
+      this.authors = res.map(author => author.name);
+    })
+  }
+
+  setIngredietns() {
+    this.ingredientService.findAll().toPromise().then(res => {
+      this.ingredients = res.map(ingredient => ingredient.name);
+    });
   }
 
   ngAfterViewInit() {
@@ -53,5 +72,22 @@ export class HomeComponent implements OnInit, AfterViewInit {
     this.recipesArray$ = this.recipeService.filterByText(
       this.filterGroup.get('text').value
     );
+  }
+
+  emitEventToChild() {
+    this.eventsSubject.next();
+  }
+
+  getValue(data: any) {
+    console.log(data);
+    if (data.name = 'ingredient' &&
+     this.ingrdeintsToFilter.indexOf(data.value) === -1) {
+      this.ingrdeintsToFilter.push(data.value);
+    }
+    console.log(this.ingrdeintsToFilter);
+  }
+
+  getIngredient() {
+    this.eventsSubject.next('ingredient');
   }
 }
