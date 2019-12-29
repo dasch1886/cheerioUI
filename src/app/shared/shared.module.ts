385f082b0eb875547247d8ba9e22bf334750051b
dasch1886 <kamil.kopacz09@gmail.com>
2019-12-29 22:58:53 +0100

    added autocomplete, search filter, click outside

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,19 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { AutocompleteInputComponent } from './autocomplete-input/autocomplete-input.component';
+import { SearchFilterPipe } from './pipe/filter/search-filter.pipe';
+import { ClickOutsideDirective } from './directive/click-outside/click-outside.directive';
 @NgModule({
-  declarations: [NavbarComponent, SpinnerComponent],
+  declarations: [NavbarComponent, SpinnerComponent, AutocompleteInputComponent, SearchFilterPipe, ClickOutsideDirective],
   imports: [
     CommonModule,
     ReactiveFormsModule
   ],
   exports: [
+    ClickOutsideDirective,
+    SearchFilterPipe,
+    AutocompleteInputComponent,
     NavbarComponent,
     SpinnerComponent,
     CommonModule,
